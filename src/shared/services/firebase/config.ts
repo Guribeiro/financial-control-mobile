46678d845b999d1ba7097cb73b58c7346842b147
Gustavo Henrique <gugahribeiro@hotmail.com>
2022-05-05 16:07:06 -0300

    feat: add enviroment variables with dotenv

--- src/shared/services/firebase/config.ts
+++ src/shared/services/firebase/config.ts
@@ -1,13 +1,22 @@
 import { FirebaseOptions } from 'firebase/app';
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEANSUREMENT_ID,
+} from '@env';
 
 const FIREBASE_CONFIG: FirebaseOptions = {
-  apiKey: 'AIzaSyDBNja4XsSkBn7pnK4xhxMGSXzkvWHLVm4',
-  authDomain: 'financial-control-a1388.firebaseapp.com',
-  projectId: 'financial-control-a1388',
-  storageBucket: 'financial-control-a1388.appspot.com',
-  messagingSenderId: '338698839332',
-  appId: '1:338698839332:web:f4b8706ffff6b91d73bfb1',
-  measurementId: 'G-DBQPGNKGY5',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEANSUREMENT_ID,
 };
 
 export default FIREBASE_CONFIG;
