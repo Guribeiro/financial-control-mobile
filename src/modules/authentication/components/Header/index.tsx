5c0ec6b42099f2de5f18aa1bfc6ce23e742a23ea
Gustavo Henrique <gugahribeiro@hotmail.com>
2022-05-06 16:49:04 -0300

    chore: add default label if it has not been defined

--- src/modules/authentication/components/Header/index.tsx
+++ src/modules/authentication/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { useTheme } from '@shared/hooks/theme';
 import { Container, GoBackButton, HeaderLabelText } from './styles';
 
 interface HeaderProps extends NativeStackHeaderProps {
-  label: string;
+  label?: string;
 }
 
 const Header = ({ label, navigation }: HeaderProps): JSX.Element => {
@@ -18,7 +18,7 @@ const Header = ({ label, navigation }: HeaderProps): JSX.Element => {
           color={customTheme.palett.colors.text_primary_100}
           size={customTheme.screen.rem(1.25, true)}
         />
-        <HeaderLabelText>{label}</HeaderLabelText>
+        <HeaderLabelText>{label || 'Voltar'}</HeaderLabelText>
       </GoBackButton>
     </Container>
   );
